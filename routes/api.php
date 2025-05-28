c4a984a64424770c7f42a0ab71ad681c7732b0f6
SetSunHeng <sunhengbrokh270405@gmail.com>
2025-05-28 15:12:25 +0700

    user-api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,5 +9,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::apiResource('students', StudentController::class);
+// Protect the students routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('students', StudentController::class);
+    //Other routes
+});
